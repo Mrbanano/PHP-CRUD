b8d21ea4484b64b8758b07f1aca51b294d164340
Alvaro <6666alvaro666@gmail.com>
2020-05-31 22:59:19 -0500

    link ref to inderx
    x

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
     <title>CRUD HTML</title>
 </head>
 <body>
@@ -10,6 +11,7 @@
     <p>lorem insup</p>
     <h2>Lorem</h2> 
     <p>lorem insup</p>
+    <script src="./static/js/index.js"></script>
 </body>
 
 
